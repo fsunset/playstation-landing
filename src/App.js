c8715e3d087b9541f0bd3d304f1db623efbdefe0
Francisco Barahona <francisco.barahona.sarmiento@gmail.com>
2020-06-12 19:39:09 -0500

    BG-img base & main logo img.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import './sass/main.scss';
+import { Container, Row, Col } from 'react-bootstrap';
+
+import ps4Logo from './img/ps4Logo.png';
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="App main-container">
+      <Container fluid className="pl-0 pr-0">
+        <Row>
+          <Col xs={12} sm={6} lg={5} xl={3}>
+            <img src={ps4Logo} alt="PS4 Logo" className="main-logo"/>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
